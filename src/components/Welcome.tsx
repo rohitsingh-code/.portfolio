d87c101a9e009e1ca70117e370f8ff330f7a5e5b
rohit <rohit19801496@@gmail.com>
2026-09-11 16:20:26 +0530

    Welcome component is completed

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,53 +1,87 @@
-// import { gsap } from "gsap/gsap-core";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap/gsap-core";
 import { useRef } from "react";
 
-// const FONT_WEIGHTS = {
-//   {
-//     min:100, max
-//   }
-// }
+const FONT_WEIGHTS = {
+  subtitle: { min: 100, max: 400, default: 100 },
+  title: { min: 400, max: 900, default: 400 }
+}
 
-const renderText = (text: any, className: any, baseWeight = 400) => {
+const renderText = (text: string, className: string, baseWeight = 400) => {
   return [...text].map((char, i) => (
-    <span key={i} className={className} style={{ fontVariationSettings: `'whgt ${baseWeight}` }}>
+    <span key={i} className={className} style={{ 
+      fontVariationSettings: `'wght' ${baseWeight}`,
+      fontWeight: baseWeight,
+      }}>
       {char === " " ? '\u00A0' : char}
     </span>
   ))
 }
 
-// const setupTextHover = (container: any, type: any) => {
-//   if (!container) return;
+const setupTextHover = (container: HTMLElement | null , type: keyof typeof FONT_WEIGHTS) => {
+  if (!container) return;
 
-//   const letters = container.querySelectorAll("span");
-//   const { min, max, default: base } = FONT_WEIGHTS[type];
+  const letters = container.querySelectorAll("span");
+  const { min, max, default: base } = FONT_WEIGHTS[type];
 
-//   const animateLetter = (letter, weight, duration = 0.25) => {
-//     return gsap.to(letter, {
-//       duration,
-//       ease: "power2.out",
-//       fontVariationSettings: `'whgt ${weight}`,
-//     });
-//   };
+  const animateLetter = (letter: HTMLSpanElement, weight: number, duration = 0.25) => {
+    return gsap.to(letter, {
+      duration,
+      ease: "power2.out",
+      fontVariationSettings: `'wght' ${weight}`,
+      fontWeight: weight,
+    });
+  };
 
-//   const handleMouseMove = (e) => {
-//     const{left} = 
-//   }
-// }
+  const handleMouseMove = (e: MouseEvent) => {
+    const{left} = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+
+    letters.forEach((letter: any) => {
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w/2));
+      const intensity = Math.exp(-(distance ** 2) / 200);
+
+      animateLetter(letter, min + (max - min) * intensity);
+    });
+  };
+
+  const handleMouseLeave = () => letters.forEach((letter) => animateLetter(letter, base, 0.3));
+
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseleave", handleMouseLeave);
+
+  return() => {
+    container.removeEventListener("mousemove", handleMouseMove);
+    container.removeEventListener("mouseleave", handleMouseLeave);
+  };
+};
+
 
 const Welcome = () => {
   const titleref = useRef(null);
   const subtitleref = useRef(null);
 
+  useGSAP(() => {
+    const titleCleanup = setupTextHover(titleref.current, "title");
+    const subtitleCleanup = setupTextHover(subtitleref.current, "subtitle");
+
+    return() => {
+      subtitleCleanup?.();
+      titleCleanup?.();
+    };
+  },[])
+
   return <section id="welcome">
     <p ref={subtitleref}>
       {renderText(
         "Hey, I'm Rohit! Welcome to my",
-        "text-3xl font genorama",
+        "text-3xl font-georama",
         100,
       )}
     </p>
     <h1 ref={titleref} className="mt-7">
-      {renderText("PortFolio", "text-9xl italic font-genorama")}
+      {renderText("PortFolio", "text-9xl italic font-georama")}
     </h1>
     
     <div className="small-screen">
