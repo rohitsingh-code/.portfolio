163ba3814d432500a1b15ed8fec790cc59c60fd9
rohit <rohit19801496@@gmail.com>
2026-09-12 19:09:36 +0530

    Improv: Added types for convinance and put constant data inside data for clearity

---
+++ src/constants/types/index.ts
@@ -0,0 +1,60 @@
+import { 
+  navLinks,
+  navIcons,
+  dockApps,
+  blogPosts,
+  techStack,
+  socials,
+  photosLinks,
+  gallery,
+
+  // WindDow Configs
+  WINDOW_CONFIG,
+  INITIAL_Z_INDEX,
+
+  // Resume and WorkEx
+  locations
+} from "../data";
+
+// Locations type
+export type Locations = typeof locations;
+export type LocationKey = keyof Locations;
+
+export type WorkLocation = typeof locations.work;
+export type AboutLocation = typeof locations.about;
+export type ResumeLocation = typeof locations.resume;
+export type TrashLocation  = typeof locations.trash;
+
+export type FileNode =
+  WorkLocation["children"][number]["children"][number];
+
+// window config
+export type WindowConfigType = typeof WINDOW_CONFIG;
+export type zIndexType = typeof INITIAL_Z_INDEX;
+
+// Components config
+export type NavLinks = typeof navLinks;
+export type NavLink = NavLinks[number];
+
+export type NavIcons = typeof navIcons;
+export type NavIcon = NavIcons[number];
+
+export type DockApps = typeof dockApps;
+export type DockApp = DockApps[number];
+
+export type BlogPosts = typeof blogPosts;
+export type BlogPost = BlogPosts[number];
+
+export type TechStacks = typeof techStack;
+export type TechStack = TechStacks[number];
+
+export type Socials = typeof socials;
+export type Social = Socials[number];
+
+export type PhotosLinks = typeof photosLinks;
+export type PhotosLink = PhotosLinks[number];
+
+export type Gallerys = typeof gallery;
+export type Gallery = Gallerys[number];
+
+
