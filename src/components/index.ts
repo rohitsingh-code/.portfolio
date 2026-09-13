4155b3c2af6cd09a0350fce2c5c50a5fd7fc3117
rohit <rohit19801496@@gmail.com>
2026-09-13 17:36:38 +0530

    Same as previous comiit changed export of DOck so changes needed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
-import { Dock } from "./Dock";
+import Dock from "./Dock";
+import WindowControlls from "./WindowControlls"
 
 export {
   Navbar,
   Welcome,
   Dock,
+  WindowControlls,
 };
